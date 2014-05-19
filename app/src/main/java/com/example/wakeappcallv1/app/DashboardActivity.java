6d89a57453f02597be7af14b58231ce99badc408
lucamarconcini <luca.m89@gmail.com>
2014-05-19 11:47:11 +0200

    Added Andrea's layout (HOME); Adapted original Login layout and activity to the rest of the app. Missing some fix.

--- app/src/main/java/com/example/wakeappcallv1/app/DashboardActivity.java
+++ app/src/main/java/com/example/wakeappcallv1/app/DashboardActivity.java
@@ -8,6 +8,8 @@ package com.example.wakeappcallv1.app;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -27,29 +29,44 @@ public class DashboardActivity extends Activity {
         userFunctions = new UserFunctions();
         if(userFunctions.isUserLoggedIn(getApplicationContext())){
             // user already logged in show databoard
-            setContentView(R.layout.dashboard);
-            btnLogout = (Button) findViewById(R.id.btnLogout);
+            setContentView(R.layout.activity_home);
 
-            btnLogout.setOnClickListener(new View.OnClickListener() {
-
-                public void onClick(View arg0) {
-                    // TODO Auto-generated method stub
-                    userFunctions.logoutUser(getApplicationContext());
-                    Intent login = new Intent(getApplicationContext(), LoginActivity2.class);
-                    login.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(login);
-                    // Closing dashboard screen
-                    finish();
-                }
-            });
 
         }else{
             // user is not logged in show login screen
-            Intent login = new Intent(getApplicationContext(), LoginActivity2.class);
+            Intent login = new Intent(getApplicationContext(), LoginActivity.class);
             login.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(login);
             // Closing dashboard screen
             finish();
         }
     }
+
+
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        int base=Menu.FIRST;
+        MenuItem item1=menu.add(base,1,1,"Log out");
+        MenuItem item2=menu.add(base,2,2,"Prova");
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId()==1){
+            userFunctions.logoutUser(getApplicationContext());
+                    Intent login = new Intent(getApplicationContext(), LoginActivity.class);
+                    login.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(login);
+                    // Closing dashboard screen
+                    finish();
+        }
+        else if (item.getItemId()==2){}
+        else return super.onOptionsItemSelected(item);
+
+        return true;
+    }
+
+
+
 }
