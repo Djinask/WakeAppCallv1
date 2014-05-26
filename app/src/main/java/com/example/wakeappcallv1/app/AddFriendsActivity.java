c8ca393aedc145b287c88d620cf4914ad5de6b3c
andreab91 <andrea.bergamini91@gmail.com>
2014-05-26 10:54:22 +0200

    Created new activity: Add Friends

---
+++ app/src/main/java/com/example/wakeappcallv1/app/AddFriendsActivity.java
@@ -0,0 +1,13 @@
+package com.example.wakeappcallv1.app;
+
+import android.app.Activity;
+import android.widget.Button;
+
+/**
+ * Created by Andrea on 26/05/14.
+ */
+public class AddFriendsActivity extends Activity {
+
+    Button addFb = (Button) findViewById(R.id.addFriendFromFb);
+    Button addName = (Button) findViewById(R.id.addFriendByName);
+}
