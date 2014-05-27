f9029b18ee1f1b8f9252284526d3c42d35bd3cdb
andreab91 <andrea.bergamini91@gmail.com>
2014-05-27 15:35:19 +0200

    list of friends

--- app/src/main/java/com/example/wakeappcallv1/app/AddFriendsActivity.java
+++ app/src/main/java/com/example/wakeappcallv1/app/AddFriendsActivity.java
@@ -1,13 +1,29 @@
 package com.example.wakeappcallv1.app;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by Andrea on 26/05/14.
  */
 public class AddFriendsActivity extends Activity {
 
-    Button addFb = (Button) findViewById(R.id.addFriendFromFb);
-    Button addName = (Button) findViewById(R.id.addFriendByName);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_friends);
+
+        Button addFb = (Button) findViewById(R.id.addFriendFromFb);
+        Button addName = (Button) findViewById(R.id.addFriendByName);
+
+        addName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+    }
 }
