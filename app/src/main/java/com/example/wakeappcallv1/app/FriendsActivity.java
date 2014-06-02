236d48a359faa09fa095a26e2d46251a46eabf4c
andreab91 <andrea.bergamini91@gmail.com>
2014-06-02 16:30:50 +0200

    Delete friends from FriendActivity (only layout)

--- app/src/main/java/com/example/wakeappcallv1/app/FriendsActivity.java
+++ app/src/main/java/com/example/wakeappcallv1/app/FriendsActivity.java
@@ -1,20 +1,30 @@
 package com.example.wakeappcallv1.app;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.wakeappcallv1.app.R;
 import com.example.wakeappcallv1.app.library.DatabaseHandler;
@@ -31,6 +41,7 @@ public class FriendsActivity extends Fragment {
     DatabaseHandler db;
     ArrayList<HashMap<String, String>> friends;
     ArrayList<String> names;
+    ArrayAdapter<String> arrayAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -58,16 +69,15 @@ public class FriendsActivity extends Fragment {
             names.add(friends.get(i).get("friendship_to"));
         }
 
-        ArrayAdapter<String> arrayAdapter =
-                new ArrayAdapter<String>(owner, R.layout.friend_list_row, R.id.textViewFriendName, names);
+        arrayAdapter = new ArrayAdapter<String>(owner, R.layout.friend_list_row, R.id.friendTextView, names);
         listView.setAdapter(arrayAdapter);
+        registerForContextMenu(listView);
 
         // click on the list item
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 // selected item
-                //String selected = ((TextView) view.findViewById(R.id.textViewList)).getText().toString();
             }
         });
 
@@ -81,8 +91,62 @@ public class FriendsActivity extends Fragment {
     }
 
     @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        String friendName = arrayAdapter.getItem(info.position);
+
+        menu.setHeaderTitle(friendName);
+        menu.add(0, v.getId(), 0, "Delete");
+    }
+
+    public boolean onContextItemSelected(MenuItem item) {
+        if(item.getTitle()=="Delete") {
+            // delete selected friend
+            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+            String friendName = arrayAdapter.getItem(info.position);
+
+            askConfirm("Confirm deletion","Are you sure you want to delete "+friendName+" from your friends?");
+        }
+        else {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         owner = activity;
     }
+
+
+    public void askConfirm(String title, String message)
+    {
+        AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+
+        alert.setTitle(title);
+        alert.setMessage(message);
+
+        // Set an EditText view to get user input
+        final EditText input = new EditText(getActivity());
+        alert.setView(input);
+
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // start thread to delete friend
+                Toast.makeText(getActivity().getApplicationContext(), "IL TUO AMICO VERRÀ CANCELLATO!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // do nothing
+            }
+        });
+
+        alert.show();
+    }
+
 }
